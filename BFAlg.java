9f62230b7972dd992e77dc849e4350a455300ac9
ZavierFang <1034052057@qq.com>
2018-12-26 15:02:22 +0800

    Update BFAlg.java

--- BFAlg.java
+++ BFAlg.java
@@ -3,29 +3,29 @@
  * 时间发咋读O(nm)
 **/
 public class BFAlg {
-	private static int bruteForce(char[] a, int n, char[] b, int m) {
-		if (m > n)
-			return -1;
+    private static int bruteForce(char[] a, int n, char[] b, int m) {
+        if (m > n)
+            return -1;
 
-		for (int i = 0; i < n - m; i++) {
-			int j = 0;
-			while (j < m && a[i + j] == b[j]) {
-				j++;
-			}
+        for (int i = 0; i < n - m; i++) {
+            int j = 0;
+            while (j < m && a[i + j] == b[j]) {
+                j++;
+            }
 
-			if (j == m) {
-				return i;
-			}
-		}
+            if (j == m) {
+                return i;
+            }
+        }
 
-		return -1;
-	}
+        return -1;
+    }
 
-	public static void main(String[] args) {
-		char[] str = "ABCDASAGAGJNNGJKNAJKSNGJKANJKGNFJKANGJANGJKNAJKSGF".toCharArray();
+    public static void main(String[] args) {
+        char[] str = "ABCDASAGAGJNNGJKNAJKSNGJKANJKGNFJKANGJANGJKNAJKSGF".toCharArray();
 
-		char[] pat = "SAGAGJNNG".toCharArray();
+        char[] pat = "SAGAGJNNG".toCharArray();
 
-		System.out.println(BFAlg.bruteForce(str, str.length, pat, pat.length));
-	}
+        System.out.println(BFAlg.bruteForce(str, str.length, pat, pat.length));
+    }
 }
